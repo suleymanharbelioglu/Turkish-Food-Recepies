c5c8791be9998b4e94c719c5630714d4b4bc7e55
suleymanharbelioglu <73783332+suleymanharbelioglu@users.noreply.github.com>
2024-10-03 15:30:44 +0300

    data prepeated

---
+++ lib/data/strings.dart
@@ -0,0 +1,243 @@
+class Strings {
+  static const foodNameList = [
+    "Easy Yellow Lentil Soup",
+    "Beyran Soup",
+    "Traditional Turkish Kofte",
+    "Sarma",
+    "Turkish Ground Beef Pide",
+    "Dried White Bean Stew",
+    "Midye Dolma",
+    "Hunkar Begendi",
+    "Turkish Kibbeh",
+    "Artichoke with Olive Oil",
+  ];
+  static const foodIngredientList = [
+    "\t1 cup yellow lentils\n\n"
+        "\t1 onion\n\n"
+        "\t3-4 tablespoons olive oil\n\n"
+        "\t5 cups of water\n\n"
+        "\t2 bay leaves\n\n"
+        "\tSalt, black pepper\n\n"
+        "\t4 tbsps of olive oil\n\n"
+        "\t1/2 tsp sweet paprika\n\n",
+    "\t1 kg of lamb neck\n\n"
+        "\t3/4 cups of rice\n\n"
+        "\t1 tablespoon lamb fat\n\n"
+        "\t1 tablespoon hot pepper flakes\n\n"
+        "\t1 tablespoon of sweet ground chili\n\n"
+        "\t3 cloves of garlic\n\n"
+        "\t2 liters of water at room temperature\n\n"
+        "\tSalt to taste\n\n",
+    "\t500g of ground beef / lamb\n\n"
+        "\t1 onion\n\n"
+        "\t1 egg\n\n"
+        "\t2 tablespoons of bread crumbs\n\n"
+        "\t1 teaspoon of black pepper\n\n"
+        "\t2 teaspoons of cumin\n\n"
+        "\t1 handful of chopped parsley\n\n"
+        "\tSalt\n\n"
+        "\tVegetable oil\n\n",
+    "\t400 g of vine leaves in brine\n\n"
+        "\t1 cup rice\n\n"
+        "\t1 tablespoon tomato paste\n\n"
+        "\t1 teaspoon red pepper powder\n\n"
+        "\t1/2 cup vegetable oil\n\n"
+        "\tSalt\n\n"
+        "\tPepper\n\n"
+        "\tWater\n\n",
+    "\t1/2 cup of lukewarm water\n\n"
+        "\t1/2 cup of lukewarm milk\n\n"
+        "\t2.5 cups of flour\n\n"
+        "\t2 tsps of active dry yeast\n\n"
+        "\t1 tbsp of sugar\n\n"
+        "\t2 tsps of sal\n\n"
+        "\t1 onion, grated\n\n"
+        "\t6 green peppers, chopped\n\n"
+        "\t2 tomatoes, diced\n\n"
+        "\tSalt to taste\n\n"
+        "\t1/2 tsp of black pepper\n\n"
+        "\t1/2 tsp of cumin\n\n",
+    "\t2 cups dried white beans\n\n"
+        "\t1 onion\n\n"
+        "\t1 tablespoon tomato paste\n\n"
+        "\t2 teaspoons paprika\n\n"
+        "\t3-4 tablespoons olive oil\n\n"
+        "\tSalt\n\n"
+        "\tWater\n\n",
+    "\t1 kg of mussels\n\n"
+        "\t1 small onion\n\n"
+        "\t1/4 cup of rice, washed and drained\n\n"
+        "\t1/8 teaspoon of tomato paste\n\n"
+        "\t1/8 teaspoon of black pepper\n\n"
+        "\t1/8 teaspoon allspice\n\n"
+        "\t1/8 teaspoon of cinnamon\n\n"
+        "\tSalt to taste\n\n"
+        "\t4-5 tablespoons of olive oil\n\n"
+        "\tWater\n\n",
+    "\t• 400 g of beef cut into bite-size pieces\n\n"
+        "\t• 1 medium onion\n\n"
+        "\t• 1 tomato\n\n"
+        "\t• 1 tablespoon of tomato paste\n\n"
+        "\t• 2 teaspoons of paprika\n\n"
+        "\t• 1 pinch of black pepper\n\n"
+        "\t• 1 pinch of cumin\n\n"
+        "\t• Salt\n\n"
+        "\t• 1/4 cup of vegetable oil\n\n"
+        "\t• 4 cups of hot water\n\n"
+        "\t• 2 eggplants\n\n"
+        "\t• 1/2 cup of water\n\n"
+        "\t• 1/2 cup of milk\n\n"
+        "\t• 1 tablespoon of flour\n\n"
+        "\t• 1 tablespoon of butter\n\n"
+        "\t• 1 handful of grated cheddar\n\n",
+    "\t• 2 cups wheat bulgur\n\n"
+        "\t• 250 g ground beef / lamb\n\n"
+        "\t• 2 tablespoons semolina\n\n"
+        "\t• 1 tablespoon flour\n\n"
+        "\t• 2.5 cups water\n\n"
+        "\t• 1 tablespoon tomato paste\n\n"
+        "\t• 1 tablespoon pepper paste\n\n"
+        "\t• 1 egg\n\n"
+        "\t• Salt\n\n"
+        "\t• 500 g ground beef / lamb\n\n"
+        "\t• 2 onions\n\n"
+        "\t• 2/3 cup olive oil\n\n"
+        "\t• 1 cup ground walnuts\n\n"
+        "\t• Salt\n\n"
+        "\t• Black pepper\n\n",
+    "\t• 4 artichoke hearts\n\n"
+        "\t• 1 onion\n\n"
+        "\t• 1 lemon\n\n"
+        "\t• 200 g of frozen mixed vegetables\n\n"
+        "\t• 4-5 tablespoons of olive oil\n\n"
+        "\t• 4 cups of water\n\n"
+        "\t• 2 teaspoons of sugar\n\n"
+        "\t• 2 teaspoons of salt\n\n"
+        "\t• 2 teaspoons of flour\n\n"
+        "\t• Minced dill\n\n"
+  ];
+  static const foodPreparationList = [
+    "\tSauté the finely chopped onions in oil until translucent\n\n"
+        "\tAdd in the rinsed lentils and stir well\n\n"
+        "\tAdd water and bay leaves, cover and simmer until the lentils are fully cooked\n\n"
+        "\tBlend the soup into a smooth consistency with an immersion blender\n\n"
+        "\tIf it's too thick add some boiling water\n\n"
+        "\tAdd in the salt and spices of your choice, boil for 1 min. more and remove from heat\n\n"
+        "\tFor seasoning heat oil in a sauce pan\n\n"
+        "\tAdd paprika, mix and remove from heat\n\n"
+        "\tWhile serving pour the seasoning over the soup\n\n",
+    "\tWash the neck and take it into the pressure cooker\n\n"
+        "\tAdd the water and the peeled garlic and cover the lid and cook until it softens\n\n"
+        "\tBoil and drain the rice in a separate saucepan and wash with cold water, drain well\n\n"
+        "\tRemove the boiled neck out of the pot, drain the broth and set aside\n\n"
+        "\tShread the meat\n\n"
+        "\tTake lamb fat into a pan\n\n"
+        "\tSprinkle with pepper flakes and ground chili\n\n"
+        "\tAdd the rice and meat\n\n"
+        "\tTake it to the stove and heat it until it sizzles on high heat\n\n"
+        "\tAdd the broth and mix\n\n"
+        "\tAdd salt according to your taste and bring to a boil and remove from heat\n\n",
+    "\t• Add the grated onion, egg, bread crumbs, parsley, salt and spices into the ground meat and stir well with your hands until you get a homogeneous mixture.\n\n"
+        "\t• Make round patties out of the mixture and press with your palm to flatten them.\n\n"
+        "\t• Heat vegetable oil in a large pan.\n\n"
+        "\t• Shallow-fry the patties on both sides.\n\n",
+    "\t• Put the leaves in water and let stand overnight,\n\n"
+        "\t• Soak with plenty of water,\n\n"
+        "\t• Take the oil and shredded onion in a sauté pan,\n\n"
+        "\t• Add tomato paste and red pepper powder and stir,\n\n"
+        "\t• Add rice and cook, stirring a few minutes,\n\n"
+        "\t• Add salt and spices you want and mix well, then remove from heat,\n\n"
+        "\t• Put a heaped teaspoon of rice mixture onto each leaf as shown on the photograph and roll,\n\n"
+        "\t• Add water just covering the rolls,\n\n"
+        "\t• Cook at low heat until soft.\n\n",
+    "\t• In a mixing bowl sift flour, yeast, sugar and salt,\n\n"
+        "\t• Add milk and water and combine well,\n\n"
+        "\t• Knead the dough until smooth and elastic and give a ball shape,\n\n"
+        "\t• Cover the dough ball with a clean damp towel and put aside for about an hour until it doubles in size,\n\n"
+        "\t• In a deep bowl combine ground beef, onion, peppers, tomatoes, salt, pepper and cumin and mix well,\n\n"
+        "\t• When the dough doubles knead for 5 minutes and cut in 6 pieces,\n\n"
+        "\t• Knead and give a ball shape to each,\n\n"
+        "\t• Flatten the balls with your hands first and then roll in an oval shape with a roller,\n\n"
+        "\t• Spread the topping on each dough evenly and fold the long sides towards inside and twist the ends,\n\n"
+        "\t• Sprinkle some flour in a cooking tray and arrange flat breads on the tray,\n\n"
+        "\t• Bake in a 190 C degrees preheated oven until the edges golden.\n\n",
+    "\t• Rinse the beans and soak in hot water, let it sit overnight,\n\n"
+        "\t• Finely chop the onion,\n\n"
+        "\t• Sauté in olive oil until tender,\n\n"
+        "\t• Add the tomato paste and stir for 2 mins,\n\n"
+        "\t• Add the paprika and stir,\n\n"
+        "\t• Drain the beans, add to the pot and stir,\n\n"
+        "\t• Add enough water to submerge the beans and cook on low heat until the beans are softened,\n\n"
+        "\t• Add salt to taste and stir.\n\n",
+    "\t• For the stuffing, chop the onion finely,\n\n"
+        "\t• Heat the olive oil in a pan, add the onion and stir-fry until soft,\n\n"
+        "\t• Add tomato paste and mix,\n\n"
+        "\t• Add the rice and mix,\n\n"
+        "\t• Add the spices and salt and mix,\n\n"
+        "\t• Add 1/2 cup of boiling water and mix,\n\n"
+        "\t• Cover and cook until soft (add extra water if necessary),\n\n"
+        "\t• Wash the outside of the mussels thoroughly by rubbing them with dishwashing wire and using a knife if necessary,\n\n"
+        "\t• Use a knife to pull the thread-like piece (beard) hanging from the inside out,\n\n"
+        "\t• Open the inside by inserting the tip of a sharp knife from the straight edge and passing the blade all around the edge,\n\n"
+        "\t• Cut off the remaining beard part in the middle,\n\n"
+        "\t• Put half a teaspoon of the stuffing mixture you prepared into the mussels and press to cover,\n\n"
+        "\t• Arrange mussels in a pan,\n\n"
+        "\t• Add enough water to cover the bottom and close the lid,\n\n"
+        "\t• Cook on low heat until the water is absorbed and the color of the mussels changes,\n\n"
+        "\t• Serve cold with lemon.\n\n",
+    "\t• Heat the vegetable oil in a pressure cooker,\n\n"
+        "\t• Add the meat and cook until it turns light brown,\n\n"
+        "\t• Add the finely chopped onions and cook until translucent,\n\n"
+        "\t• Add the tomato paste and paprika and stir for 2-3 mins,\n\n"
+        "\t• Add the chopped tomato and cook until softened,\n\n"
+        "\t• Add the black pepper, cumin and water, then stir,\n\n"
+        "\t• Put the lid on and cook according to the instructions of your pressure cooker,\n\n"
+        "\t• Open the lid, add the salt and continue to boil in order to reduce the sauce, until you reach the desired consistency,\n\n"
+        "\t• Roast the eggplants either in the oven or over the stove,\n\n"
+        "\t• Peel off the skin and finely chop,\n\n"
+        "\t• In a saucepan melt the butter,\n\n"
+        "\t• Add the flour and stir for 3 mins,\n\n"
+        "\t• Add the eggplants and stir for 1-2 mins,\n\n"
+        "\t• Add the milk, water and salt and cook constantly stirring until thickened,\n\n"
+        "\t• Remove from heat, add the cheddar and stir,\n\n"
+        "\t• On a serving platter, first pour the eggplant sauce and then pour the meat sauce on top.\n\n",
+    "\t• Finely mince the onion,\n\n"
+        "\t• Heat the olive oil in a pan,\n\n"
+        "\t• Sauté the onion until soft,\n\n"
+        "\t• Add the ground meat and cook until it turns brown,\n\n"
+        "\t• Add the ground walnuts, salt and black pepper, stir and remove from heat,\n\n"
+        "\t• In a separate pan, add the bulgur, semolina and water,\n\n"
+        "\t• Cook on low heat until all the water is absorbed,\n\n"
+        "\t• On a large tray, mix the bulgur mixture with all the other outer crust ingredients and knead well for a few minutes with your hands or until everything is well combined,\n\n"
+        "\t• Grease your hands with oil and pull clementine size pieces from the mixture, then form them into smooth balls,\n\n"
+        "\t• Form an indent in the centre of each ball ensuring that the crust is as thin as possible,\n\n"
+        "\t• Fill the indent with the meat mixture and mold the crust around the filling,\n\n"
+        "\t• In a deep frying pan, heat the vegetable oil,\n\n"
+        "\t• Drop the koftes in and deep-fry until golden brown on all sides.\n\n",
+    "\t• Squeeze the lemon and add the juice into the water in a large bowl, then soak the artichoke hearts into the water,\n\n"
+        "\t• Stir together the flour, salt and sugar in a small bowl along with 1 cup of the water/lemon mixture,\n\n"
+        "\t• Finely chop the onion,\n\n"
+        "\t• Sauté in a large pot with olive oil until softened,\n\n"
+        "\t• Add in the flour/water mixture and stir,\n\n"
+        "\t• Place the artichoke in the pot,\n\n"
+        "\t• Finally add in the rest of the water,\n\n"
+        "\t• Cook on low heat until the artichoke is softened,\n\n"
+        "\t• Fill the artichoke hearts with frozen vegetable mix,\n\n"
+        "\t• Spoon over some of the cooking water,\n\n"
+        "\t• Put the lid on and cook for 5 mins, then remove from heat,\n\n"
+        "\t• Let it sit (lid on) until completely cooled down,\n\n"
+        "\t• Serve with minced dill.\n\n"
+  ];
+  static const foodImageNameList = [
+    "easyYellowLentilSoup",
+    "beyranSoup",
+    "TraditionalTurkishKofte",
+    "Sarma",
+    "TurkishGroundBeefPide",
+    "DriedWhiteBeanStew",
+    "MidyeDolma",
+    "HunkarBegendi",
+    "TurkishKibbeh",
+    "ArtichokewithOliveOil",
+  ];
+}
